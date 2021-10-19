11e91569ecaa165b9c4036464424946891f8c9bc
tka-andrew <kui.an.andrew.tan@gmail.com>
2021-10-19 23:14:20 +0800

    LeetCode1143 using DP

---
+++ Cpp/LeetCode1143.LongestCommonSubsequence_DP.cpp
@@ -0,0 +1,33 @@
+// using Dynamic Programming
+
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+    int longestCommonSubsequence(std::string text1, std::string text2) {
+        int m = text1.size();
+        int n = text2.size();
+        
+        std::vector<std::vector<int>> dp(m+1, std::vector<int>(n+1));
+        
+        for (int i=0; i<m; i++) {
+            dp[i][0] = 0;
+        }
+        
+        for (int j=0; j<n; j++) {
+            dp[0][j] = 0;
+        }
+        
+        for (int i=1; i<=m; i++) {
+            for (int j=1; j<=n; j++) {
+                if (text1[i-1] == text2[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                } else {
+                    dp[i][j] = std::max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
