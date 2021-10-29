cd45049ebb16e3806d07bdb32430a04ac5c7ffff
tka-andrew <kui.an.andrew.tan@gmail.com>
2021-10-29 23:55:27 +0800

    LeetCode10 using DP but with time limit exceeded

---
+++ Cpp/LeetCode10.RegularExpressionMatching_toBeImproved.cpp
@@ -0,0 +1,43 @@
+#include <string>
+
+class Solution {
+public:
+    bool isMatch(std::string s, std::string p) {
+        return dp(s,0,p,0);
+    }
+    
+    bool dp(std::string s, int i, std::string p, int j) {
+        int sSize = s.size();
+        int pSize = p.size();
+        while (i<sSize && j<pSize) {
+            if (s[i] == p[j] || p[j] == '.') {
+                if(j<pSize-1 && p[j+1]=='*') {
+                    return dp(s,i,p,j+2) || dp(s,i+1,p,j); // match 0 time or more than 1 time
+                } else {
+                    return dp(s, i+1, p, j+1); // match 1
+                }
+            } else {
+                if (j<pSize-1 && p[j+1]=='*') {
+                    return dp(s, i , p, j+2); // match 0 time
+                } else {
+                    return false;          
+                }
+            }
+        }
+        if (j==pSize) {
+            return i==sSize;
+        }
+        if (i==sSize) {
+            if((pSize-j)%2==1) {
+                return false;
+            }
+            
+            for (;j+1<pSize;j+=2) {
+                if(p[j+1]!='*') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
