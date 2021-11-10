5773c04fba28170ca6e9e3c5f2f098a7c2aec671
tka-andrew <kui.an.andrew.tan@gmail.com>
2021-11-11 01:44:09 +0800

    LeetCode297 with timeLimitExceeded problem

---
+++ Cpp/LeetCode297.SerializeAndDeserializeBinaryTree_withTimeLimitError.cpp
@@ -0,0 +1,72 @@
+#include <queue>
+#include <string>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+    // Encodes a tree to a single string.
+    std::string serialize(TreeNode* root) {
+        std::string s = "";
+        if (root == nullptr) {
+            return s;
+        }
+        serializeHelper(root, s);
+        return s;
+    }
+    
+    void serializeHelper(TreeNode* root, std::string& s) {
+        if (root == nullptr) {
+            s = s + "#,";
+            return;
+        }
+        s = s + std::to_string(root->val) + ",";
+        serializeHelper(root->left, s);
+        serializeHelper(root->right, s);
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(std::string data) {
+        int stringSize = data.size();
+        if(stringSize==0)
+            return nullptr;
+        std::queue<std::string> q;
+        stringstream str(data);
+        std::string val;
+        while (true) {
+            getline(str,val,',');
+            if (val == "") {
+                break;
+            }
+            q.push(val);
+        }
+        return deserializeHelper(q);
+    }
+    
+    TreeNode* deserializeHelper(std::queue<std::string>& q) {
+        if (q.size()==0){
+            return nullptr;
+        }
+        std::string val = q.front();
+        q.pop();
+        
+        if (val == "#") {
+            return nullptr;
+        }
+        TreeNode* root = new TreeNode(stoi(val));
+        root->left = deserializeHelper(q);
+        root->right = deserializeHelper(q);
+        return root;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec ser, deser;
+// TreeNode* ans = deser.deserialize(ser.serialize(root));
