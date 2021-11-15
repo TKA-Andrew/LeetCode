1d470c8fb580c8c6f8e2ba8c57dc1e88ae47cf1f
tka-andrew <kui.an.andrew.tan@gmail.com>
2021-11-16 00:00:04 +0800

    leetcode 108 bst

---
+++ Cpp/LeetCode108.ConvertSortedArrayToBinarySearchTree_BST.cpp
@@ -0,0 +1,34 @@
+// time complexity: O(N)
+// space complexity: O(N)
+
+#include <vector>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return constructMinHeightBST(nums, 0, nums.size() -1);
+    }
+    
+    TreeNode* constructMinHeightBST(vector<int>& nums, int startIdx, int endIdx) {
+        if (endIdx<startIdx) {
+            return nullptr;
+        }
+        
+        int midIdx = (startIdx + endIdx)/2;
+        TreeNode* root = new TreeNode(nums[midIdx]);
+        root->left = constructMinHeightBST(nums, startIdx, midIdx-1);
+        root->right = constructMinHeightBST(nums, midIdx+1, endIdx);
+        return root;
+    }
+};
