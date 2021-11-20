57ea81402196ce680c913ea296f17254804820d4
tka-andrew <kui.an.andrew.tan@gmail.com>
2021-11-20 21:25:16 +0800

    LeetCode445 by reversing linked list

---
+++ Cpp/LeetCode445.AddTwoNumbersII_reverseLinkedList.cpp
@@ -0,0 +1,53 @@
+// time complexity: O(max(m,n))
+// space complexity: O(max(m,n))
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int carry = 0;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        auto l1copy = reverse(l1);
+        auto l2copy = reverse(l2);
+        int carry=0;
+        auto ans = new ListNode(0);
+        auto currentNode = ans;
+        while (l1copy != nullptr || l2copy != nullptr || carry != 0) {
+            int value1 = l1copy == nullptr ? 0 : l1copy->val;
+            int value2 = l2copy == nullptr ? 0 : l2copy->val;
+            int sum = value1 + value2 + carry;
+            int leftover = sum%10;
+            carry = sum/10;
+            auto newNode = new ListNode(leftover);
+            currentNode->next = newNode;
+            currentNode = newNode; // point to next node
+            
+            l1copy = l1copy == nullptr ? nullptr:l1copy->next;
+            l2copy = l2copy == nullptr ? nullptr:l2copy->next;
+            
+        }
+        return reverse(ans->next);
+    }
+    
+    ListNode* reverse(ListNode* l) {
+        ListNode* prev = nullptr;
+        auto currentNode = l;
+        auto temp = l->next;
+        while (currentNode != nullptr) {
+            temp = currentNode->next;
+            currentNode->next = prev;
+            prev = currentNode;
+            currentNode = temp;
+        }
+        return prev;
+    }
+    
+};
