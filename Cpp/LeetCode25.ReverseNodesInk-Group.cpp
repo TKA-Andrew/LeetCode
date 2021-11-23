d0ac502cb3bdb8fce5ca3842349635bebcec1e40
tka-andrew <kui.an.andrew.tan@gmail.com>
2021-11-23 23:14:42 +0800

    LeetCode25 Linked List with recursion

---
+++ Cpp/LeetCode25.ReverseNodesInk-Group.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (head == nullptr) {
+            return nullptr;
+        }
+        ListNode* a = head;
+        ListNode* b = head;
+        for (int i=0; i<k; i++) {
+            if (b==nullptr) {
+                return head;
+            }
+            b=b->next;
+        }
+        
+        ListNode* newhead = reverse(a,b);
+        a->next = reverseKGroup(b,k);
+        return newhead;
+        
+    }
+    
+    ListNode* reverse(ListNode* a, ListNode* b) {
+        ListNode* prev = nullptr;
+        while (a!=b) {
+            auto temp = a->next;
+            a->next = prev;
+            prev = a;
+            a=temp;
+        }
+        return prev; // reversed head
+    }
+};
