5573a8924f66ab664f3dfdd205000292d931c911
tka-andrew <kui.an.andrew.tan@gmail.com>
2022-01-02 02:54:46 +0800

    updated explanation

--- Cpp/LeetCode322.CoinChange_DP.cpp
+++ Cpp/LeetCode322.CoinChange_DP.cpp
@@ -1,22 +1,56 @@
 // This is a bottom up approach using Dynamic Programming
-// Time Complexity: O(nk) , where k is numbder of coins
+// Time Complexity: O(nk) , where k is number of coins
 // Space Complexity: O(n)
 
-#include <algorithm> // for min()
-#include <vector>
+#include <vector>   
 
 class Solution {
 public:
     int coinChange(std::vector<int>& coins, int amount) {
-        std::vector<int> dp(amount + 1, amount + 1);
-        // dp[i] means the minimum number of coins needed to achieve the target i amount
-        dp[0] = 0; // base case
-        for (int i = 1; i <= amount; i++) {
-            for (auto coin : coins) {
-                if ((i - coin) < 0) continue; // invalid to proceed
+        // DEFINITION OF DP
+        /*
+            dp(i) function returns the minimum number of coin needed to achieve target i amount
+            For example:
+                input: coins = [2,5], amount = 11
+                dp(0) = 0; // BASE CASE
+                dp(1) = -1; // impossible
+                dp(2) = 1; // 2
+                dp(3) = -1; // impossible
+                dp(4) = -1; // impossible
+                dp(5) = 1; // 1
+                dp(6) = -1; // impossible
+                dp(7) = 2 // 5 + 2
+                dp(8) = -1; // impossible
+                dp(9) = 3; // 5 + 2 + 2
+                dp(10) = -1; // impossible
+                dp(11) = 4 // 5 + 2 + 2 + 2
+        */
+        
+        std::vector<int> dp(amount+1, amount+1);
+        // here we actually can initialize all elements to INT_MAX
+        // but amount+1 is enough to be the top boundary
+        
+        // BASE CASE
+        dp[0] = 0;
+        
+        // CHOICES
+        // - coins[i] for(int i=0;i<N;i++) if (target-coins[i] > 0)
+        
+        // STATE TRANSITION
+        /*
+        dp[i] = { 0, i = 0
+                { min(dp[i], dp[i-coins[j]]+1), if (i-coins[j]) >= 0
+        */
+        
+        for (int i=0;i<=amount;i++) {
+            for (const auto coin:coins) {
+                if (i - coin < 0) {
+                    continue; // invalid to proceed
+                }
                 dp[i] = std::min(dp[i], dp[i-coin] + 1); // + 1 is for the coin used in "- coin" expression
             }
         }
+        
         return (dp[amount] == amount + 1) ? -1 : dp[amount];
     }
 };
