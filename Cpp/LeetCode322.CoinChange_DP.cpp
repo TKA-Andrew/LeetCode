e6cb3051b3d5b0498622bbf58afb474d843bb08d
tka-andrew <kui.an.andrew.tan@gmail.com>
2022-01-06 07:57:47 +0800

    correction made

--- Cpp/LeetCode322.CoinChange_DP.cpp
+++ Cpp/LeetCode322.CoinChange_DP.cpp
@@ -1,4 +1,3 @@
-// This is a bottom up approach using Dynamic Programming
 // Time Complexity: O(nk) , where k is number of coins
 // Space Complexity: O(n)
 
@@ -9,26 +8,28 @@ public:
     int coinChange(std::vector<int>& coins, int amount) {
         // DEFINITION OF DP
         /*
-            dp(i) function returns the minimum number of coin needed to achieve target i amount
+            d[i] means the minimum number of coin needed to achieve target i amount
             For example:
                 input: coins = [2,5], amount = 11
-                dp(0) = 0; // BASE CASE
-                dp(1) = -1; // impossible
-                dp(2) = 1; // 2
-                dp(3) = -1; // impossible
-                dp(4) = -1; // impossible
-                dp(5) = 1; // 1
-                dp(6) = -1; // impossible
-                dp(7) = 2 // 5 + 2
-                dp(8) = -1; // impossible
-                dp(9) = 3; // 5 + 2 + 2
-                dp(10) = -1; // impossible
-                dp(11) = 4 // 5 + 2 + 2 + 2
+                dp[0] = 0; // BASE CASE
+                dp[1]= amount+1; // impossible
+                dp[2] = 1; // 2
+                dp[3] = amount+1; // impossible
+                dp[4] = 2; // 2+2
+                dp[5] = 1; // 1
+                dp[6] = 3; // 2+2+2
+                dp[7] = 2 // 5 + 2
+                dp[8] = 4; // 2+2+2+2
+                dp[9] = 3; // 5 + 2 + 2
+                dp[10] = 2; // 5 + 5
+                dp[11] = 4 // 5 + 2 + 2 + 2
         */
+        // final answer = dp[amount]
         
         std::vector<int> dp(amount+1, amount+1);
         // here we actually can initialize all elements to INT_MAX
         // but amount+1 is enough to be the top boundary
+        // and using INT_MAX will have problem when later comes to dp[i-coin] + 1
         
         // BASE CASE
         dp[0] = 0;
@@ -39,6 +40,7 @@ public:
         // STATE TRANSITION
         /*
         dp[i] = { 0, i = 0
+                { INT_MAX, if (i-coins[j])<0
                 { min(dp[i], dp[i-coins[j]]+1), if (i-coins[j]) >= 0
         */
         
