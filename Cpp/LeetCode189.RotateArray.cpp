357dd08db833e991113d66df67370f09e409a4fd
tka-andrew <kui.an.andrew.tan@gmail.com>
2022-01-24 22:43:21 +0800

    LeetCode189

---
+++ Cpp/LeetCode189.RotateArray.cpp
@@ -0,0 +1,15 @@
+// O(N) time
+// O(1) space
+
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    void rotate(std::vector<int>& nums, int k) {
+        k = k % nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
+    }
+};
