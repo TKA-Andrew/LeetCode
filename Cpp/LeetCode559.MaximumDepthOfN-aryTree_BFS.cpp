00da6d3b54a7cb8dcf74cee6dcffd6cc9a6c558a
tka-andrew <kui.an.andrew.tan@gmail.com>
2022-07-06 19:15:38 +0800

    LeetCode559 BFS

---
+++ Cpp/LeetCode559.MaximumDepthOfN-aryTree_BFS.cpp
@@ -0,0 +1,53 @@
+#include <queue>
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        std::queue<Node*> q;
+        int depth = 0;
+        if (root == nullptr) return 0;
+        
+        q.push(root);
+        
+        while(!q.empty())
+        {
+            int n = q.size();
+            for (int i=0; i<n; ++i)
+            {
+                auto node = q.front();
+                q.pop();
+                if (node->children.size() == 0)
+                {
+                    continue;
+                } else {
+                    for (auto child:node->children)
+                    {
+                        q.push(child);
+                    }
+                }
+            }
+            ++depth;
+        }
+        return depth;
+    }
+};
