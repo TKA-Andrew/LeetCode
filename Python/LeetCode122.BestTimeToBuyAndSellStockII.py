602dace22e034d02449978f683bf96f7ae9f480b
tka-andrew <kui.an.andrew.tan@gmail.com>
2023-04-06 23:27:45 +0800

    LeetCode122 python

---
+++ Python/LeetCode122.BestTimeToBuyAndSellStockII.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        N = (len(prices))
+
+        left = 0
+        right = 1
+        maxProfit = 0
+
+        while right < N:
+            if (prices[left] < prices[right]):
+                maxProfit += (prices[right] - prices[left])
+            left = right
+            right += 1
+        return maxProfit
