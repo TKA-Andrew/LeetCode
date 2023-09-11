86711e38d767a68341b7be7a05e2e83817d7b5bd
tka-andrew <kui.an.andrew.tan@gmail.com>
2023-09-11 22:39:59 +0800

    LeetCode20 python

---
+++ Python/LeetCode20.ValidParentheses.py
@@ -0,0 +1,20 @@
+# Time Complexity: O(N)
+# Space Complexity: O(N)
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        bracket_map = {
+            ')': '(',
+            '}': '{',
+            ']': '[',
+        }
+        for char in s:
+            if char in bracket_map.values():
+                stack.append(char)
+            elif char in bracket_map.keys():
+                if not stack or stack.pop() != bracket_map[char]:
+                    return False
+            else:
+                return False
+        return not stack
